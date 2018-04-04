ada97ecabc387723c7494e17256171e66feac136
Teodor Tomescu <tomescut@mcmaster.ca>
2018-04-04 16:20:44 -0400

    converted dropdowns, fixed constraints, added user input capabilities

--- app/src/main/java/ca/mcmaster/tomescut/botany/Search.java
+++ app/src/main/java/ca/mcmaster/tomescut/botany/Search.java
@@ -2,17 +2,29 @@ package ca.mcmaster.tomescut.botany;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class Search extends AppCompatActivity {
+
+    public String flowerShapeResult;
+    String clusterTypeResult;
+    String leafShapeResult;
+    String bloomTimeResult;
+    String useCaseRsult;
+    String sizeResult;
+    String locationResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
 
         Spinner clusterTypeSpinner = (Spinner) findViewById(R.id.clusterTypeSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -56,6 +68,130 @@ public class Search extends AppCompatActivity {
 // Apply the adapter to the spinner
         useCaseSpinner.setAdapter(adapter5);
 
+        flowerShapeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+                if (i == 1) {
+                    flowerShapeResult = "irregular";
+                } else if (i == 2) {
+                    flowerShapeResult = "three";
+                } else if (i == 3) {
+                    flowerShapeResult = "four";
+                } else if (i == 4) {
+                    flowerShapeResult = "five";
+                } else if (i == 5) {
+                    flowerShapeResult = "six";
+                } else if (i == 6) {
+                    flowerShapeResult = "seven";
+                } else if (i == 7) {
+                    flowerShapeResult = "eightOrMore";
+                } else if (i == 8) {
+                    flowerShapeResult = "dontKnow";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        clusterTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+                if (i == 1) {
+                    clusterTypeResult = "individual";
+                } else if (i == 2) {
+                    clusterTypeResult = "elongate";
+                } else if (i == 3) {
+                    clusterTypeResult = "flatOrRound";
+                } else if (i == 4) {
+                    clusterTypeResult = "dontknow";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        leafShapeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+                if (i == 1) {
+                    leafShapeResult = "smooth";
+                } else if (i == 2) {
+                    leafShapeResult = "toothed";
+                } else if (i == 3) {
+                    leafShapeResult = "lobed";
+                } else if (i == 4) {
+                    leafShapeResult = "thinRays";
+                } else if (i == 5) {
+                    leafShapeResult = "mixed";
+                } else if (i == 6) {
+                    leafShapeResult = "dontKnow";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        bloomTimeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+                if (i == 1) {
+                    bloomTimeResult = "aprToMay";
+                } else if (i == 2) {
+                    bloomTimeResult = "aprToJune";
+                } else if (i == 3) {
+                    bloomTimeResult = "aprToNov";
+                } else if (i == 4) {
+                    bloomTimeResult = "mayToJuly";
+                } else if (i == 5) {
+                    bloomTimeResult = "juneToAug";
+                } else if (i == 6) {
+                    bloomTimeResult = "juneToSept";
+                } else if (i == 7) {
+                    bloomTimeResult = "juneToNov";
+                } else if (i == 8) {
+                    bloomTimeResult = "dontKnow";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        useCaseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long id) {
+                if (i == 1) {
+                    bloomTimeResult = "medicinal";
+                } else if (i == 2) {
+                    bloomTimeResult = "edible";
+                } else if (i == 3) {
+                    bloomTimeResult = "symbolism";
+                } else if (i == 4) {
+                    bloomTimeResult = "perfume";
+                } else if (i == 5) {
+                    bloomTimeResult = "floristry";
+                } else if (i == 6) {
+                    bloomTimeResult = "dontKnow";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
